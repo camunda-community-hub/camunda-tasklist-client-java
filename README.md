cd3333c9a7cc5d57112a6898d563cc080e33b537
Christophe <christophe.dame@camunda.com>
2022-07-14 09:43:50 +0200

    bugfix : Avoid crashing on no variables or null valued variables

--- README.md
+++ README.md
@@ -59,7 +59,7 @@ You can import it to your maven or gradle project as a dependency
 <dependency>
 	<groupId>io.camunda</groupId>
 	<artifactId>camunda-tasklist-client-java</artifactId>
-	<version>1.1.0</version>
+	<version>1.1.1</version>
 </dependency>
 ```
 # Troubleshooting
--- src/main/java/io/camunda/tasklist/util/ApolloUtils.java
+++ src/main/java/io/camunda/tasklist/util/ApolloUtils.java
@@ -75,8 +75,10 @@ public class ApolloUtils {
     public static Task toTask(Object apolloTask) throws TaskListException {
         try {
             Task task = getObjectMapper().readValue(getObjectMapper().writeValueAsString(apolloTask), Task.class);
-            for(io.camunda.tasklist.dto.Variable var : task.getVariables()) {
-                improveVariable(var);
+            if (task.getVariables()!=null) {
+                for(io.camunda.tasklist.dto.Variable var : task.getVariables()) {
+                    improveVariable(var);
+                }
             }
             return task;
         } catch (JsonProcessingException e) {
@@ -97,10 +99,12 @@ public class ApolloUtils {
     public static List<VariableInput> toVariableInput(Map<String, Object> variablesMap) {
         List<VariableInput> variables = new ArrayList<>();
         for (Map.Entry<String, Object> entry : variablesMap.entrySet()) {
-            if (entry.getValue() instanceof String) {
-                variables.add(new VariableInput(entry.getKey(), '"' + String.valueOf(entry.getValue()) + '"'));
-            } else {
-                variables.add(new VariableInput(entry.getKey(), String.valueOf(entry.getValue())));
+            if (entry.getValue()!=null) {
+                if (entry.getValue() instanceof String) {
+                    variables.add(new VariableInput(entry.getKey(), '"' + (String) entry.getValue() + '"'));
+                } else {
+                    variables.add(new VariableInput(entry.getKey(), String.valueOf(entry.getValue())));
+                }
             }
         }
         return variables;
