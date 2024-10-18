d05c7c665f99ad1fe693a40da70dc987d851ae40
Jonathan Lukas <jonathan.lukas@camunda.com>
2024-10-18 06:30:11 +0200

    ignore unknown fields

--- extension/client-java/src/main/java/io/camunda/tasklist/auth/TokenResponse.java
+++ extension/client-java/src/main/java/io/camunda/tasklist/auth/TokenResponse.java
@@ -1,7 +1,9 @@
 package io.camunda.tasklist.auth;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class TokenResponse {
 
   @JsonProperty("access_token")
